515f3bb5e606df64027d83f0c5f41b649cfed537
Javeed <javeedshaik7346@gmail.com>
2025-10-04 12:12:05 +0530

    setup.py file ready

---
+++ setup.py
@@ -0,0 +1,29 @@
+from typing import List
+from setuptools import find_packages,setup
+
+def get_requirements()->List[str]:
+    requirements_lst:List[str]=[]
+    try:
+        with open("requirements.txt") as file:
+            # Read lines from the file 
+            lines=file.readlines()
+            # Process each line 
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!='-e .':
+                    requirements_lst.append(requirement)
+            
+
+
+    except FileNotFoundError:
+        print("File not Found")
+    return requirements_lst
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Javeed",
+    author_email="dspexam123@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
