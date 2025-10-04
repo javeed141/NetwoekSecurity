967e94dce5a41ea85276c63764a53923a55b692b
Javeed <javeedshaik7346@gmail.com>
2025-10-04 12:16:14 +0530

    setup.py file ready

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements()->List[str]:
 setup(
     name="NetworkSecurity",
     version="0.0.1",
-    author="Javeed",
+    author="JaveedShaik",
     author_email="dspexam123@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
